ecfcf1035e695c9c5799b9ee5f46df295eccfdab
dhinadk28 <dhinadk28@gmail.com>
2023-02-24 21:10:07 +0530

    add

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://socialmedia-backend-mndy.onrender.com";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
+  BASE_URL = "https://socialmedia-backend-mndy.onrender.com";
 }
 
 export { BASE_URL };
